0ba2db63819a14f89561da82cc5b94c6fb0d0169
Selim Ozel <sozel@wpi.edu>
2020-09-10 18:09:55 +0300

    Example added to compute yearly and monthly volatility of TIP.

---
+++ source/examples/tip_std/app.d
@@ -0,0 +1,37 @@
+import std.stdio;
+import std.datetime;
+import std.string;
+import std.conv;
+import std.math: sqrt, pow;
+
+// Miner class
+import YahooFinanceD;
+
+void main()
+{
+	// Example: 
+	// 1- Mine TIP between September 9, 2019 and September 9, 2020
+	// 2- Compute year-to-day standard deviation
+	// 3- Obtain monthly standard deviation
+	string name = "TIP";
+	Date begin = Date(2019, 9, 9);
+	Date end = Date(2020, 9, 9);
+
+	YahooFinanceD simpleMiner;
+	simpleMiner.Mine(begin, end, name, intervals.daily); // scrape monthly
+	Frame[] TIP = simpleMiner.Write!(output.frame, logger.off, Frame[]); // write to data frame with logging
+
+	double sum = 0;
+	double mean = 0;
+	double std = 0;
+	int n = TIP.length;
+	for(int i = 0; i<TIP.length; i++) { sum += TIP[i].price.close; }
+	mean = sum/n;
+
+	sum = 0;
+	for(int i = 0; i<TIP.length; i++) { sum += pow((TIP[i].price.close-mean),2); }
+	std = sum/n;
+
+	writeln("Yearly volatility of TIP between 2019 and 2020: "~to!string(std));
+	writeln("Monthly volatility of TIP between 2019 and 2020: "~to!string(std/sqrt(12.0)));
+}
