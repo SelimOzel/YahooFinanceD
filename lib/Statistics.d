5c7b07ba4628fe1648f7d0a970561a744804baa4
Selim Ozel <sozel@wpi.edu>
2021-03-11 21:30:16 +0100

    Added functions descriptions and a place holder for OLS.

--- lib/Statistics.d
+++ lib/Statistics.d
@@ -1,6 +1,9 @@
 import std.math: sqrt, pow;
 import std.exception: enforce;
 
+/* 
+	Mean value of a time series
+*/ 
 double compute_mean(double[] array_IN) pure {
 	double sum = 0;
 	int n = array_IN.length;
@@ -8,6 +11,9 @@ double compute_mean(double[] array_IN) pure {
 	return sum/n;
 }
 
+/*
+	Standard deviation of a time series
+*/
 double compute_std(double[] array_IN) pure {
 	double sum = 0;
 	int n = array_IN.length;
@@ -15,6 +21,9 @@ double compute_std(double[] array_IN) pure {
 	return sum/n;	
 }
 
+/* 
+	Correlation between two time series.
+*/ 
 double compute_correlation(double[] array_x_IN, double[] array_y_IN) pure {
 	enforce(array_x_IN.length == array_y_IN.length, "compute_correlation: not same length"); 
 	int n = array_x_IN.length;
@@ -27,4 +36,15 @@ double compute_correlation(double[] array_x_IN, double[] array_y_IN) pure {
 		denominator += sqrt(pow(array_x_IN[i]-x_mean, 2)*pow(array_y_IN[i]-y_mean, 2));
 	}
 	return numerator/denominator;
+}
+
+/* 
+	y_t = alpha + rho*x_t + e_t
+	Computes rho_est and alpha_est based on n observations given time series x_t and y_t where e_t is random noise
+*/ 
+double[2] compute_ordinary_least_squares() {
+	double rho_est = 0.0;
+	double alpha_est = 0.0;
+
+	return [rho_est, alpha_est];
 }
