56fad6cb173ae445977b65e5c208f5a72c4a489d
selim <sozel@wpi.edu>
2023-12-14 21:49:20 -0500

    Added a test script for the new feature

--- ci.sh
+++ ci.sh
@@ -2,6 +2,7 @@ dub run yahoofinanced:example-csv
 dub run yahoofinanced:example-dataframe
 dub run yahoofinanced:example-gold
 dub run yahoofinanced:example-json
+dub run yahoofinanced:example-pairs
 dub run yahoofinanced:example-soxx_analysis
 dub run yahoofinanced:example-tip_std
 dub test yahoofinanced:unittest
